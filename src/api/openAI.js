772264ac9fb338e3c7df026bdc4a047804d22b8a
AjithChitturi <119422118+AjithChitturi@users.noreply.github.com>
2023-12-15 23:24:28 +0530

    Update openAI.js

--- src/api/openAI.js
+++ src/api/openAI.js
@@ -1,88 +1,131 @@
-import { apiKey } from "../constants";
+/* eslint-disable prettier/prettier */
+/* eslint-disable comma-dangle */
+/* eslint-disable prettier/prettier */
+import { apiKey } from '../constants';
 import axios from 'axios';
+
 const client = axios.create({
     headers: {
-        "Authorization": "Bearer "+apiKey,
-        "Content-Type": "application/json",
+        'Authorization': 'Bearer ' + apiKey,
+        'Content-Type': 'application/json'
     }
-})
+});
 
 const chatgptUrl = 'https://api.openai.com/v1/chat/completions';
-const dalleUrl = 'https://api.openai.com/v1/images/generations';
 
-export const apiCall = async (prompt, messages)=>{
-    
-    // // Logic 1 : this will check the prompt from chatgpt if user wants to create an image
-    try{
+export const apiCall = async (prompt, messages) => {
+    try {
         const res = await client.post(chatgptUrl, {
-            model: "gpt-3.5-turbo",
+            model: 'gpt-3.5-turbo',
             messages: [{
                 role: 'user',
-                content: `Does this message want to generate an AI picture, image, art or anything similar? ${prompt} . Simply answer with a yes or no.`,
-            }],
+                content:` Does this message pertain to anything related to healthcare, medicine, drugs, or similar topics? ${prompt}. Simply answer with a yes or no.`
+            }]
         });
-        isArt = res.data?.choices[0]?.message?.content;
-        isArt = isArt.trim();
-        if(isArt.toLowerCase().includes('yes')){
-            console.log('dalle api call');
-            return dalleApiCall(prompt, messages)
-        }else{
-            console.log('chatgpt api call')
-            return chatgptApiCall(prompt, messages);
+
+        let isHealthRelated = res.data?.choices[0]?.message?.content;
+        isHealthRelated = isHealthRelated.trim().toLowerCase();
+
+        if (isHealthRelated.includes('yes')) {
+            const relatedWords = ['tablet', 'pill', 'infection', 'disease', 'pain', 'ache'];
+            const lowerPrompt = prompt.toLowerCase();
+
+            let category = '';
+
+            if (relatedWords.some(word => lowerPrompt.includes(word))) {
+                if (lowerPrompt.includes('tablet') || lowerPrompt.includes('pill')) {
+                    category = 'tablet';
+                } else if (lowerPrompt.includes('infection') || lowerPrompt.includes('disease')) {
+                    category = 'disease';
+                } else if (lowerPrompt.includes('pain') || lowerPrompt.includes('ache')) {
+                    category = 'pain';
+                }
+            } else {
+                category = 'general';
+            }
+
+            const response = await getChatResponseForCategory(category, prompt, messages);
+            return response;
+        } else {
+            console.log('Non health-related content');
+            return { success: false, msg: 'There is no data for the question you have asked.' };
         }
 
-    }catch(err){
-        console.log('error: ',err);
-        return Promise.resolve({success: false, msg: err.message});
+    } catch (err) {
+        console.log('error: ', err);
+        return { success: false, msg: err.message };
     }
+};
 
-    // // Logic 2 : sometimes chatgpt does not understand the art messages but thats fine, you can use this approach :)
+const getChatResponseForCategory = async (category, prompt, messages) => {
+    try {
+        let responsePrompt = '';
 
-    // prompt = prompt.toLowerCase();
-    // let isArt = prompt.includes('image') || prompt.includes('sketch') || prompt.includes('art') || prompt.includes('picture') || prompt.includes('drawing');
-    // if(isArt){
-    //     console.log('dalle api call');
-    //     return dalleApiCall(prompt, messages)
-    // }else{
-    //     console.log('chatgpt api call')
-    //     return chatgptApiCall(prompt, messages);
-    // }
-    
-}
+        const relatedWords = ['tablet', 'pill', 'infection', 'disease', 'pain', 'ache'];
+        const lowerPrompt = prompt.toLowerCase();
 
-const chatgptApiCall = async (prompt, messages)=>{
-    try{
+        let determinedCategory = '';
+
+        if (relatedWords.some(word => lowerPrompt.includes(word))) {
+            if (lowerPrompt.includes('tablet') || lowerPrompt.includes('pill')) {
+                determinedCategory = 'tablet';
+            } else if (lowerPrompt.includes('infection') || lowerPrompt.includes('disease')) {
+                determinedCategory = 'disease';
+            } else if (lowerPrompt.includes('pain') || lowerPrompt.includes('ache')) {
+                determinedCategory = 'pain';
+            }
+        } else {
+            determinedCategory = 'general';
+        }
+
+        switch (determinedCategory) {
+            case 'tablet':
+                responsePrompt = 'How to use tablet? Side effects of the tablet and prevention from side effects.';
+                break;
+            case 'disease':
+                responsePrompt = 'Related tablets to use for the disease.';
+                break;
+            case 'pain':
+                responsePrompt = 'What are the tablets to use for pain?';
+                break;
+            case 'general':
+                responsePrompt = 'Precautions to be taken for a general problem.';
+                break;
+            default:
+                responsePrompt = 'Could not understand the specific category.';
+        }
+
+        const messageToApiCall = [
+            ...messages,
+            { role: 'user', content: prompt }, // User's original query
+            { role: 'user', content: responsePrompt } // Response prompt based on category
+        ];
+
+        const res = await chatgptApiCall(responsePrompt, messageToApiCall);
+        return res;
+
+    } catch (err) {
+        console.log('error: ', err);
+        return { success: false, msg: err.message };
+    }
+};
+
+const chatgptApiCall = async (prompt, messages) => {
+    try {
         const res = await client.post(chatgptUrl, {
-            model: "gpt-3.5-turbo",
-            messages,
-        })
+            model: 'gpt-3.5-turbo',
+            messages: messages.concat([{ role: 'user', content: prompt }]),
+            temperature: 0.7,
+        });
 
-        let answer = res.data?.choices[0]?.message?.content;
-        messages.push({role: 'assistant', content: answer.trim()});
-        // console.log('got chat response', answer);
-        return Promise.resolve({success: true, data: messages}); 
+        const answer = res.data?.choices[0]?.message?.content;
+        messages.push({ role: 'assistant', content: answer.trim() });
 
-    }catch(err){
-        console.log('error: ',err);
-        return Promise.resolve({success: false, msg: err.message});
+        return { success: true, data: messages };
+
+    } catch (err) {
+        console.log('error: ', err);
+        return { success: false, msg: err.message };
     }
-}
+};
 
-const dalleApiCall = async (prompt, messages)=>{
-    try{
-        const res = await client.post(dalleUrl, {
-            prompt,
-            n: 1,
-            size: "512x512",
-        })
-
-        let url = res?.data?.data[0]?.url;
-        // console.log('got image url: ',url);
-        messages.push({role: 'assistant', content: url});
-        return Promise.resolve({success: true, data: messages});
-
-    }catch(err){
-        console.log('error: ',err);
-        return Promise.resolve({success: false, msg: err.message});
-    }
-}
