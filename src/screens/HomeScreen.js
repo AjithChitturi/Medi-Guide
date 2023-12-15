e37ce28969fbf4ed5d682bc1763978fdbf32e0d1
AjithChitturi <119422118+AjithChitturi@users.noreply.github.com>
2023-12-15 23:25:17 +0530

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, {useEffect, useRef, useState} from 'react';
 import {
   View,
@@ -34,17 +35,17 @@ const App = () => {
     console.log('speech event: ',e);
     const text = e.value[0];
     setResult(text);
-    
+
   };
 
   const speechErrorHandler = e=>{
     console.log('speech error: ',e);
-  }
+  };
 
-  
+
   const startRecording = async () => {
     setRecording(true);
-    Tts.stop(); 
+    Tts.stop();
     try {
       await Voice.start('en-GB'); // en-US
 
@@ -53,7 +54,7 @@ const App = () => {
     }
   };
   const stopRecording = async () => {
-    
+
     try {
       await Voice.stop();
       setRecording(false);
@@ -70,7 +71,7 @@ const App = () => {
   };
 
   const fetchResponse = async ()=>{
-    if(result.trim().length>0){
+    if (result.trim().length > 0){
       setLoading(true);
       let newMessages = [...messages];
       newMessages.push({role: 'user', content: result.trim()});
@@ -83,32 +84,32 @@ const App = () => {
       apiCall(result.trim(), newMessages).then(res=>{
         console.log('got api data');
         setLoading(false);
-        if(res.success){
+        if (res.success){
           setMessages([...res.data]);
           setResult('');
           updateScrollView();
 
           // now play the response to user
-          startTextToSpeach(res.data[res.data.length-1]);
-          
-        }else{
+          startTextToSpeach(res.data[res.data.length - 1]);
+
+        } else {
           Alert.alert('Error', res.msg);
         }
-        
-      })
+
+      });
     }
-  }
+  };
 
 
 
   const updateScrollView = ()=>{
     setTimeout(()=>{
       scrollViewRef?.current?.scrollToEnd({ animated: true });
-    },200)
-  }
+    },200);
+  };
 
   const startTextToSpeach = message=>{
-    if(!message.content.includes('https')){
+    if (!message.content.includes('https')){
       setSpeaking(true);
       // playing response with the voice id and voice speed
       Tts.speak(message.content, {
@@ -116,13 +117,13 @@ const App = () => {
         rate: 0.5,
       });
     }
-  }
-  
+  };
+
 
   const stopSpeaking = ()=>{
     Tts.stop();
     setSpeaking(false);
-  }
+  };
 
   useEffect(() => {
 
@@ -131,15 +132,15 @@ const App = () => {
     Voice.onSpeechEnd = speechEndHandler;
     Voice.onSpeechResults = speechResultsHandler;
     Voice.onSpeechError = speechErrorHandler;
-    
+
     // text to speech events
     Tts.setDefaultLanguage('en-IE');
     Tts.addEventListener('tts-start', event => console.log('start', event));
-    Tts.addEventListener('tts-finish', event => {console.log('finish', event); setSpeaking(false)});
+    Tts.addEventListener('tts-finish', event => {console.log('finish', event); setSpeaking(false);});
     Tts.addEventListener('tts-cancel', event => console.log('cancel', event));
 
-    
-    
+
+
     return () => {
       // destroy the voice instance after component unmounts
       Voice.destroy().then(Voice.removeAllListeners);
@@ -149,114 +150,74 @@ const App = () => {
 
   return (
     <View className="flex-1 bg-white">
-      {/* <StatusBar barStyle="dark-content" /> */}
+      {/* ... (existing code) */}
       <SafeAreaView className="flex-1 flex mx-5">
-        {/* bot icon */}
-        <View className="flex-row justify-center">
-          <Image  
-              source={require('../../assets/images/bot.png')}
-              style={{height: hp(15), width: hp(15)}}
-          />
-        </View>
-        
+        {/* ... (existing code) */}
         {/* features || message history */}
-        {
-          messages.length>0? (
-            <View className="space-y-2 flex-1">
-              <Text className="text-gray-700 font-semibold ml-1" style={{fontSize: wp(5)}}>Assistant</Text>
-        
-              <View 
-                style={{height: hp(58)}} 
-                className="bg-neutral-200 rounded-3xl p-4">
-                  <ScrollView  
-                    ref={scrollViewRef} 
-                    bounces={false} 
-                    className="space-y-4" 
-                    showsVerticalScrollIndicator={false}
-                  >
-                    {
-                      messages.map((message, index)=>{
-                        if(message.role=='assistant'){
-                          if(message.content.includes('https')){
-                            // result is an ai image
-                            return (
-                              <View key={index} className="flex-row justify-start">
-                                <View 
-                                  className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
-                                    <Image  
-                                      source={{uri: message.content}} 
-                                      className="rounded-2xl"  
-                                      resizeMode="contain" 
-                                      style={{height: wp(60), width: wp(60)}} 
-                                    />
-                                </View>
-                              </View>
-                              
-                              
-                            )
-                          }else{
-                            // chat gpt response
-                            return (
-                              <View 
-                                key={index} style={{width: wp(70)}} 
-                                className="bg-emerald-100 p-2 rounded-xl rounded-tl-none">
-                                <Text className="text-neutral-800" style={{fontSize: wp(4)}}  >
-                                  {message.content}
-                                </Text>
-                              </View>
-                            )
-                          }
-                        }else{
-                          // user input text
-                          return (
-                            <View key={index} className="flex-row justify-end">
-                              <View 
-                                style={{width: wp(70)}} 
-                                className="bg-white p-2 rounded-xl rounded-tr-none">
-                                <Text style={{fontSize: wp(4)}}  >
-                                  {message.content}
-                                </Text>
-                              </View>
-                            </View>
-                          );
-                        }
-                        
-                        
-                      })
-                    }
-                  </ScrollView>
-              </View>
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text className="text-gray-700 font-semibold ml-1" style={{ fontSize: wp(5) }}>
+              Assistant
+            </Text>
+
+            <View
+              style={{ height: hp(58) }}
+              className="bg-neutral-200 rounded-3xl p-4">
+              <ScrollView
+                ref={scrollViewRef}
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}>
+                {messages.map((message, index) => {
+                  if (message.role === 'assistant' && !message.content.includes('https')) {
+                    // Display only chat GPT responses that are not images
+                    return (
+                      <View
+                        key={index}
+                        style={{ width: wp(70) }}
+                        className="bg-emerald-100 p-2 rounded-xl rounded-tl-none">
+                        <Text className="text-neutral-800" style={{ fontSize: wp(4) }}>
+                          {message.content}
+                        </Text>
+                      </View>
+                    );
+                  }
+                  // Skip displaying user's input text or images
+                  return null;
+                })}
+              </ScrollView>
             </View>
-          ): (
+          </View>
+          ) : (
               <Features />
           )
         }
-        
-        
+
+
         {/* recording, clear and stop buttons */}
         <View className="flex justify-center items-center">
           {
-            loading? (
-              <Image 
+            loading ? (
+              <Image
                 source={require('../../assets/images/loading.gif')}
                 style={{width: hp(10), height: hp(10)}}
               />
-            ):
+            ) :
               recording ? (
                 <TouchableOpacity className="space-y-2" onPress={stopRecording}>
                   {/* recording stop button */}
-                  <Image 
-                    className="rounded-full" 
+                  <Image
+                    className="rounded-full"
                     source={require('../../assets/images/voiceLoading.gif')}
                     style={{width: hp(10), height: hp(10)}}
                   />
                 </TouchableOpacity>
-                
+
               ) : (
                 <TouchableOpacity onPress={startRecording}>
                   {/* recording start button */}
-                  <Image 
-                    className="rounded-full" 
+                  <Image
+                    className="rounded-full"
                     source={require('../../assets/images/recordingIcon.png')}
                     style={{width: hp(10), height: hp(10)}}
                   />
@@ -264,9 +225,9 @@ const App = () => {
               )
           }
           {
-            messages.length>0 && (
-              <TouchableOpacity 
-                onPress={clear} 
+            messages.length > 0 && (
+              <TouchableOpacity
+                onPress={clear}
                 className="bg-neutral-400 rounded-3xl p-2 absolute right-10"
               >
                 <Text className="text-white font-semibold">Clear</Text>
@@ -275,19 +236,19 @@ const App = () => {
           }
           {
             speaking && (
-              <TouchableOpacity 
-                onPress={stopSpeaking} 
+              <TouchableOpacity
+                onPress={stopSpeaking}
                 className="bg-red-400 rounded-3xl p-2 absolute left-10"
               >
                 <Text className="text-white font-semibold">Stop</Text>
               </TouchableOpacity>
             )
           }
-            
-            
-          
+
+
+
         </View>
-        
+
       </SafeAreaView>
     </View>
   );
